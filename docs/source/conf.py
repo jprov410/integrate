76a68df040b57e4361d19966fd0a18dde66f8303
Justin Provazza <jprov410@gmail.com>
2018-01-23 16:22:11 -0500

    back to napoleon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'numpydoc',
-#    'sphinx.ext.napoleon',
+#    'numpydoc',
+    'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode']
 
