c8303c92487eeccbcf8b9a6d1a42c3755d46557c
Eliseo Marin <meliseo@vt.edu>
2018-01-23 12:57:20 -0500

    Indentation correction

--- integrate/stochastic/monte_carlo.py
+++ integrate/stochastic/monte_carlo.py
@@ -12,10 +12,10 @@ def monte_1d(x, f, trials):
    return (b-a) * np.sum(y) / trials
 
 def monte_2d(f, v, domain, trials):
-    area = np.prod(domain[1] - domain[0])
-    matrix = np.sqrt(area) * np.random.rand(trials, 2) + np.min(domain)
-    v_eval = v(matrix) 
-    idx = np.where(v_eval < 1.0)
-    points_inside = v_eval[idx]
-    k = points_inside.size
-    return area * k / trials
+   area = np.prod(domain[1] - domain[0])
+   matrix = np.sqrt(area) * np.random.rand(trials, 2) + np.min(domain)
+   v_eval = v(matrix) 
+   idx = np.where(v_eval < 1.0)
+   points_inside = v_eval[idx]
+   k = points_inside.size
+   return area * k / trials
