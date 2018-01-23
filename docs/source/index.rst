7ee356288b2976448802cf68c5c1bb99c59edbcb
Justin Provazza <jprov410@gmail.com>
2018-01-23 16:06:07 -0500

    Added an HTML documentation scheme

---
+++ docs/source/index.rst
@@ -0,0 +1,24 @@
+.. Integrates documentation master file, created by
+   sphinx-quickstart on Tue Jan 23 15:34:25 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Integrates's documentation!
+======================================
+
+HERE IS THE SMALL LIBRARY OF CODES FOR INTEGRATION
+
+.. autofunction:: integrate.newton_cotes.simpson.evaluate
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- integrate/newton_cotes/simpson.py
+++ integrate/newton_cotes/simpson.py
@@ -4,12 +4,36 @@ This file contains the implementation of the Simpson rule
 
 import numpy as np
 
-def evaluate(x, f):
-    a = x[0]
-    b = x[1]
-    ya = f(a)
-    yb = f((a+b)/2)
-    yc = f(b)
-    I = (b-a) * (ya + 4 * yb + yc) / 6.
+def evaluate(bounds , func):
+    """
+
+    Evaluates simpsons rules on an array of values and a funtion pointer
+
+    .. math::
+          
+        \int_{a}^{b} = \sum_i ...
+
+    parameters
+    ----------
+    bounds : array_like
+        An array with dimension of 2 that contains the integration bounds
+    func : function
+        integrand to be evaluated
+
+    returns
+    -------
+    integral : float
+        The integral of the function on the domain specified by bounds
+
+    """
+    if len(bounds) != 2:
+        raise ValueError("Bounds should contain 2 elements, found %d." % len(bounds))
+
+    a = bounds[0]
+    b = bounds[1]
+    ya = func(a)
+    yb = func((a+b)/2.)
+    yc = func(b)
+    I = (b-a) * (ya + 4. * yb + yc) / 6.
     return I
 
